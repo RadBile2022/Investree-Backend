1f9c63ebbfcc11e00ebed37cfdf3bbfaa995f879
RadBile2022 <wahradar511@gmail.com>
2022-10-30 15:11:39 +0700

    soal 4. controller

--- investree-task5-master/src/main/java/com/investree/demo/repository/TransaksiRepo.java
+++ investree-task5-master/src/main/java/com/investree/demo/repository/TransaksiRepo.java
@@ -3,5 +3,9 @@ package com.investree.demo.repository;
 import com.investree.demo.model.Transaksi;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Map;
+
 public interface TransaksiRepo extends JpaRepository<Transaksi,Long> {
+    public Map update(Transaksi transaksi);
+
 }
